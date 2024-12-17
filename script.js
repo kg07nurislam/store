e6c312f84ebade656801139a22fc3ec5f073979d
kg07nurislam <152377115+kg07nurislam@users.noreply.github.com>
2024-12-17 19:22:36 +0600

    Update script.js
    Signed-off-by: kg07nurislam  <152377115+kg07nurislam@users.noreply.github.com>

--- script.js
+++ script.js
@@ -65,3 +65,12 @@ function confirmQuantity() {
 
   closeModal();
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
